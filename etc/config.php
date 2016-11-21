965ff0405f191b40e1ad01aea3382897d14f239b
JaeHo Song <thruthesky@gmail.com>
2016-11-21 21:19:30 +0900

    Empty database

--- etc/config.php
+++ etc/config.php
@@ -1,5 +1,5 @@
 <?php
 
 define('DEVELOPMENT', false);
-define('SERVER_URL', 'http://w8.philgo.com/etc/xbase/index.php');
-// define('SERVER_URL', 'http://work.org/xbase/index.php');
+//define('SERVER_URL', 'http://w8.philgo.com/etc/xbase/index.php');
+define('SERVER_URL', 'http://work.org/xbase/index.php');
